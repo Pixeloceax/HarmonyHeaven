3dd153114b104c784591808befaddf24db1dec61
pisha <pisha@hotmail.fr>
2024-01-25 16:22:49 +0100

    [INIT] - getCurrentUser post request first init

--- frontend/src/services/auth.service.ts
+++ frontend/src/services/auth.service.ts
@@ -1,11 +1,15 @@
 import axios from "axios";
-
-const BACKEND_URL="https://127.0.0.1:8000";
-const LOGIN="/login"
+import IUser from "../types/use.type";
 
 class AuthService {
+
+  private readonly BACKEND_URL="https://127.0.0.1:8000";
+  private readonly LOGIN="/login"
+  private readonly GET_USER_DATA = "/get-current-user"
+
+
   async login(email: string, password: string) {
-    const response = await axios.post(BACKEND_URL+LOGIN, {
+    const response = await axios.post(this.BACKEND_URL+this.LOGIN, {
       email,
       password,
     });
@@ -26,7 +30,7 @@ class AuthService {
     adress: string,
     phone: string
   ) {
-    return axios.post(BACKEND_URL+LOGIN + "signup", {
+    return axios.post(this.BACKEND_URL+this.LOGIN + "signup", {
       username,
       email,
       password,
@@ -35,12 +39,24 @@ class AuthService {
     });
   }
 
-  getCurrentUser() {
-    const userStr = localStorage.getItem("user");
-    if (userStr) return JSON.parse(userStr);
-
-    return null;
-  }
+  async getCurrentUser(): Promise<IUser | null> {
+    return new Promise(async (resolve, reject) => {
+      try {
+        const token: string | null =  localStorage.getItem("user");
+        if (!token) {
+          reject(new Error('User email not found in local storage'));
+          return;
+        }
+        const email = JSON.parse(token).user
+        const response = await axios.post(`${this.BACKEND_URL}${this.GET_USER_DATA}`, {
+          email
+        });
+        resolve(response.data);
+      } catch (error) {
+        reject(error);
+      }
+    });
+  }  
 }
 
 export default new AuthService();
