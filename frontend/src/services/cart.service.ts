a65a945b27aaa6a710d63c56fd8ab66e9c5a9608
Axel <axel.valentinbvs@yahoo.com>
2024-02-07 16:18:19 +0100

    feat(cart func): add service for cart

---
+++ frontend/src/services/cart.service.ts
@@ -0,0 +1,86 @@
+import IProduct from "../types/product.type";
+import axios from "axios";
+import { jwtDecode } from "jwt-decode";
+
+interface ICartItem {
+  product: IProduct;
+  quantity: number;
+}
+
+class cartService {
+  private readonly BACKEND_URL = "http://localhost:8000";
+  private readonly SUBMIT_CART = "/cart";
+
+  private setCart(cart: ICartItem[]) {
+    localStorage.setItem("cart", JSON.stringify(cart));
+  }
+
+  getCart(): ICartItem[] {
+    return JSON.parse(localStorage.getItem("cart") as string) || [];
+  }
+
+  addToCart(
+    productId: string,
+    productName: string,
+    productImage: string,
+    productPrice: number
+  ) {
+    const cart = this.getCart();
+
+    const foundSameProduct = cart.find((p) => p.product.id === productId);
+
+    if (foundSameProduct) {
+      foundSameProduct.quantity += 1;
+    } else {
+      cart.push({
+        product: {
+          id: productId,
+          name: productName,
+          image: productImage,
+          price: productPrice,
+        },
+        quantity: 1,
+      });
+    }
+
+    this.setCart(cart);
+  }
+
+  updateCartQuantityItem(productId: string, quantity: number) {
+    const cart = this.getCart();
+    const updatedCart = cart.map((item) =>
+      item.product.id === productId ? { ...item, quantity } : item
+    );
+    this.setCart(updatedCart);
+  }
+
+  removeFromCart(productId: string) {
+    const cart = this.getCart();
+    const updatedCart = cart.filter((item) => item.product.id !== productId);
+    this.setCart(updatedCart);
+  }
+
+  async confirmCart() {
+    const cart = this.getCart();
+    const products = cart.map((item) => ({
+      productId: item.product.id,
+      quantity: item.quantity,
+    }));
+
+    const userId = jwtDecode(localStorage.getItem("user") as string).id;
+    const response = await axios.post(
+      `${this.BACKEND_URL}${this.SUBMIT_CART}`,
+      {
+        products,
+        userId,
+      }
+    );
+
+    if (response.status === 200) {
+      localStorage.removeItem("cart");
+    }
+    return response;
+  }
+}
+
+export default new cartService();
