4ca48fa22a2a2516ecc3d40059949148c7b9ab8d
Axel <axel.valentinbvs@yahoo.com>
2024-02-15 16:11:50 +0100

    save for merge

--- frontend/src/components/Navbar/Navbar.component.tsx
+++ frontend/src/components/Navbar/Navbar.component.tsx
@@ -1,9 +1,13 @@
 import "./Narvbar.css";
 import { MdClose } from "react-icons/md";
-import { CgMenuMotion } from "react-icons/cg";
+import { IoMenu } from "react-icons/io5";
+import { IoCart } from "react-icons/io5";
+
+import logo from "../../assets/icons/png/LOGO sans texte.png";
 
 const Navbar = () => {
-  const NavLink = [
+  const navLinks = [
+    "home",
     "shop",
     "orders",
     "login",
@@ -11,50 +15,64 @@ const Navbar = () => {
     "about",
     "logout",
     "register",
+    "contact",
   ];
 
   function openNav() {
-    const myNav = document.getElementById("myNav");
-    if (myNav) {
-      myNav.style.width = "100%";
+    const navElement = document.getElementById("navElement");
+    if (navElement) {
+      navElement.style.width = "100%";
     }
   }
 
   function closeNav() {
-    const myNav = document.getElementById("myNav");
-    if (myNav) {
-      myNav.style.width = "0%";
+    const navElement = document.getElementById("navElement");
+    if (navElement) {
+      navElement.style.width = "0%";
     }
   }
 
   return (
     <>
-      <div id="myNav" className="overlay">
-        <button className="closebtn" onClick={closeNav}>
+      <nav className="navbar">
+        <h1>
+          <a className="navbar-logo" href="/">
+            <img src={logo} alt="logo" />
+          </a>
+        </h1>
+        <div className="navbar-link-container">
+          <ul className="navbar-list">
+            <li className="navbar-item">
+              <a href="/cart">
+                <IoCart className="icon" />
+              </a>
+            </li>
+            <li className="navbar-item">
+              <button className="navbar-open-button" onClick={openNav}>
+                <IoMenu className="icon" />
+              </button>
+            </li>
+          </ul>
+        </div>
+      </nav>
+
+      <div id="navElement" className="navbar-overlay">
+        <button className="navbar-close-button" onClick={closeNav}>
           <MdClose />
         </button>
 
-        <div className="overlay-content">
-          {NavLink.map((link, index) => (
-            <li className="link-li" key={index}>
-              <a className="link-effect" href={`/${link}`}>
-                {link.toUpperCase()}
-              </a>
-            </li>
-          ))}
+        <div className="navbar-overlay-content">
+          <ul>
+            {navLinks.map((link, index) => (
+              <li className="navbar-overlay-link-item" key={index}>
+                <a className="navbar-overlay-link" href={`/${link}`}>
+                  {link.toUpperCase()}
+                </a>
+              </li>
+            ))}
+          </ul>
         </div>
       </div>
-
-      <nav className="navbar-container">
-        <h1>
-          <a className="logo" href="/">
-            Navbar
-          </a>
-        </h1>
-        <button className="openbtn" onClick={openNav}>
-          <CgMenuMotion />
-        </button>
-      </nav>
     </>
   );
 };
