2b8dafb8f054fcbf9be1d763fb6e712b3d2e6158
Axel <axel.valentinbvs@yahoo.com>
2024-02-16 01:36:47 +0100

    fix(cart): add product to cart

--- frontend/src/pages/shop/Shop.tsx
+++ frontend/src/pages/shop/Shop.tsx
@@ -1,14 +1,15 @@
-import * as React from 'react';
-import Pagination from '@mui/material/Pagination';
-import PaginationItem from '@mui/material/PaginationItem';
-import Stack from '@mui/material/Stack';
-import ArrowBackIcon from '@mui/icons-material/ArrowBack';
-import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
+import * as React from "react";
+import Pagination from "@mui/material/Pagination";
+import PaginationItem from "@mui/material/PaginationItem";
+import Stack from "@mui/material/Stack";
+import ArrowBackIcon from "@mui/icons-material/ArrowBack";
+import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
 import IProduct from "../../types/product.type";
+import cartService from "../../services/cart.service";
 import axios from "axios";
 import "./shop.css";
 
-type Props = object;  
+type Props = object;
 type State = {
   currentPage: number;
   productsPerPage: number;
@@ -44,18 +45,19 @@ export default class Vinyls extends React.Component<Props, State> {
   // Handle adding item to cart
   handleAddToCart(productId: number) {
     // Send request to add product to cart
-    axios.post('http://127.0.0.1:8000/cart', {
-      productId: productId,
-      quantity: 1, // You can adjust the quantity as needed
-    })
-    .then(response => {
-      console.log('Item added to cart:', response.data);
-      // Handle success if needed (e.g., show a message to the user)
-    })
-    .catch(error => {
-      console.error('Error adding item to cart:', error);
-      // Handle error if needed
-    });
+    axios
+      .post("http://127.0.0.1:8000/cart", {
+        productId: productId,
+        quantity: 1, // You can adjust the quantity as needed
+      })
+      .then((response) => {
+        console.log("Item added to cart:", response.data);
+        // Handle success if needed (e.g., show a message to the user)
+      })
+      .catch((error) => {
+        console.error("Error adding item to cart:", error);
+        // Handle error if needed
+      });
   }
 
   render() {
@@ -64,7 +66,9 @@ export default class Vinyls extends React.Component<Props, State> {
     // Pagination Logic
     const indexOfLastProduct = currentPage * productsPerPage;
     const indexOfFirstProduct = indexOfLastProduct - productsPerPage;
-    const currentProducts = products ? products.slice(indexOfFirstProduct, indexOfLastProduct) : [];
+    const currentProducts = products
+      ? products.slice(indexOfFirstProduct, indexOfLastProduct)
+      : [];
 
     // Change page
     const handleChange = (event: React.ChangeEvent<unknown>, value: number) => {
@@ -82,8 +86,19 @@ export default class Vinyls extends React.Component<Props, State> {
                 <p>{product.artist}</p>
                 <p>{product.price}€</p>
                 <div className="buttons-div">
-                  <button >favorite</button>
-                   <button onClick={() => this.handleAddToCart(product.id)}>card</button>
+                  <button>favorite</button>
+                  <button
+                    onClick={() =>
+                      cartService.addToCart(
+                        product.id,
+                        product.name as string,
+                        product.image,
+                        product.price as number
+                      )
+                    }
+                  >
+                    card
+                  </button>
                 </div>
               </div>
             ))}
@@ -92,15 +107,14 @@ export default class Vinyls extends React.Component<Props, State> {
         <div className="pagination">
           <Stack spacing={2}>
             <Pagination
-              className='vinyls-pagination'
-              count={products ? Math.ceil(products.length / productsPerPage) : 1}
+              className="vinyls-pagination"
+              count={
+                products ? Math.ceil(products.length / productsPerPage) : 1
+              }
               page={currentPage}
               onChange={handleChange}
               renderItem={(item) => (
-                <PaginationItem
-                  component="button"
-                  {...item}
-                />
+                <PaginationItem component="button" {...item} />
               )}
               prevIcon={<ArrowBackIcon />}
               nextIcon={<ArrowForwardIcon />}
