7541558b710741a25e28eb084714aeda6756b2ea
Axel <axel.valentinbvs@yahoo.com>
2024-02-19 10:21:13 +0100

    refactor: change getCurrentUser from POST to GET, get user id from token, improve error handling for login

--- backend/harmonyheaven/src/Controller/LoginController.php
+++ backend/harmonyheaven/src/Controller/LoginController.php
@@ -9,6 +9,8 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Lexik\Bundle\JWTAuthenticationBundle\Services\JWTTokenManagerInterface;
+use Lexik\Bundle\JWTAuthenticationBundle\Encoder\JWTEncoderInterface;
+
 
 class LoginController extends AbstractController
 {
@@ -18,46 +20,61 @@ class LoginController extends AbstractController
     public function login(Request $request, UserRepository $userRepository, UserPasswordHasherInterface $passwordHasher, JWTTokenManagerInterface $jwtManager): JsonResponse
     {
         $data = json_decode($request->getContent(), true);
-    
+
+        if (!isset($data['email']) || !isset($data['password'])) {
+            return new JsonResponse(['error' => 'Missing email or password'], JsonResponse::HTTP_BAD_REQUEST);
+        }
+
         $email = $data['email'];
         $password = $data['password'];
         $user = $userRepository->findOneBy(['email' => $email]);
-    
-    
-        // Initialize $token with a default value
-        if ($user && $passwordHasher->isPasswordValid($user, $password)) {
-            $payload = [
-                'email'=> $user->getEmail(),
-                'id' => $user->getId(),
-            ];
-            $token = $jwtManager->createFromPayload($user, $payload);
+
+        if (!$user) {
+            return new JsonResponse(['error' => 'Invalid password or email'], JsonResponse::HTTP_NOT_FOUND);
         }
-    
+
+        if (!$passwordHasher->isPasswordValid($user, $password)) {
+            return new JsonResponse(['error' => 'Invalid password or email'], JsonResponse::HTTP_UNAUTHORIZED);
+        }
+
+        $payload = [
+            'email' => $user->getEmail(),
+            'id' => $user->getId(),
+        ];
+        $token = $jwtManager->createFromPayload($user, $payload);
+
         return new JsonResponse([
             'user' => $token,
         ]);
     }
-    
 
     /**
-     * @Route("/get-current-user", name="get-current-user", methods={"POST"})
+     * @Route("/get-current-user", name="get-current-user", methods={"GET"})
      */
-    public function getCurrentUser(Request $request, UserRepository $userRepository): JsonResponse
-    {
-        $data = json_decode($request->getContent(), true);
+    public function getCurrentUser(
+        Request $request,
+        UserRepository $userRepository,
+        JWTTokenManagerInterface $jwtManager,
+        JWTEncoderInterface $jwtEncoder,
+    ): JsonResponse {
 
-        $email = $data['email'];
-        $token = $request->headers->get('Authorization');
-        $user = $userRepository->findOneBy(['email' => $email]);
+        $authHeader = $request->headers->get('Authorization');
+        $authToken = str_replace('Bearer ', '', $authHeader);
+        $decodedJwtToken = $jwtEncoder->decode($authToken);
 
-        if(!$user || $token === null || !$email || $email == null) {
+        $userId = $decodedJwtToken['id'];
+
+        $user = $userRepository->findOneBy(['id' => $userId]);
+
+
+        if (!$user || $authHeader === null || !$userId || $userId == null) {
             return new JsonResponse(['error' => 'Une erreur est survenue.'], JsonResponse::HTTP_BAD_REQUEST);
         }
 
         return new JsonResponse([
-            'user'  => $user->getUserIdentifier(),
+            'user' => $user->getUserIdentifier(),
             'roles' => $user->getRoles(),
-            'name'=> $user->getName(),
+            'name' => $user->getName(),
             'address' => $user->getAddress(),
             'phone' => $user->getPhone(),
         ]);
