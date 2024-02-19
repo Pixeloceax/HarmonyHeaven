753c33442b444898d4ed0487f4083bfae6548e7e
Axel <axel.valentinbvs@yahoo.com>
2024-02-19 10:44:45 +0100

    feat: create genre service get all genre with axios

---
+++ frontend/src/services/genre.service.ts
@@ -0,0 +1,20 @@
+import axios from "axios";
+import IGenre from "../types/genre.type";
+
+class genreService {
+  private readonly BACKEND_URL = "http://localhost:8000";
+  private readonly PRODUCTS_LIST = "/genres-list";
+
+  async getGenres() {
+    try {
+      const response = await axios.get<IGenre[]>(
+        `${this.BACKEND_URL}${this.PRODUCTS_LIST}`
+      );
+      return response.data;
+    } catch (err) {
+      throw new Error(err as string);
+    }
+  }
+}
+
+export default new genreService();
