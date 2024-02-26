5e9bbda0fe87cb93c41313064f5f57bc83a18f5c
Axel <axel.valentinbvs@yahoo.com>
2024-02-26 16:50:27 +0100

    feat: get all product

--- backend/harmonyheaven/src/Controller/AdminBoardController.php
+++ backend/harmonyheaven/src/Controller/AdminBoardController.php
@@ -4,69 +4,91 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use App\Repository\UserRepository;
-use Lexik\Bundle\JWTAuthenticationBundle\Encoder\JWTEncoderInterface;
-use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
-use Lexik\Bundle\JWTAuthenticationBundle\Services\JWTTokenManagerInterface;
-use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
+use App\Repository\ProductRepository;
+use App\Repository\GenreRepository;
+use App\Repository\StyleRepository;
 
 #[IsGranted('ROLE_ADMIN', message: 'Only admin can access this route')]
 class AdminBoardController extends AbstractController
 {
+
+    // ---------- USER CRUD ADMIN ----------
+
     /**
-     * @Route("/admin/board", name="adminBoard", methods={"PUT"})
+     * @Route("/admin/users", name="getAllUsers", methods={"GET"})
      */
-    public function ManageUsersInformations(
-        Request $request,
-        JWTEncoderInterface $jwtEncoder,
-        EntityManagerInterface $entityManager,
-        UserRepository $userRepository,
-        UserPasswordHasherInterface $passwordHasher,
-        JWTTokenManagerInterface $jwtManager
-    ): JsonResponse {
-        $authHeader = $request->headers->get('Authorization');
-        $authToken = str_replace('Bearer ', '', $authHeader);
-        $decodedJwtToken = $jwtEncoder->decode($authToken);
-        $userId = $decodedJwtToken['id'];
-        $user = $userRepository->findOneBy(['id' => $userId]);
+    public function getAllUsers(UserRepository $userRepository): JsonResponse
+    {
+        $users = $userRepository->findAll();
 
-        if (!$user) {
-            return new JsonResponse(['message' => 'User not found',], 404);
+        $usersArray = [];
+        foreach ($users as $user) {
+            if (
+                $user->getRoles() != [
+                    "ROLE_ADMIN",
+                    "ROLE_USER"
+                ]
+            ) {
+                $usersArray[] = [
+                    'name' => $user->getName(),
+                    'user' => $user->getEmail(),
+                    'address' => $user->getAddress(),
+                    'phone' => $user->getPhone(),
+                ];
+            }
         }
-
-        $data = json_decode($request->getContent(), true);
-
-        $this->updateUserInformation($user, $data, $passwordHasher);
-
-        $entityManager->persist($user);
-        $entityManager->flush();
-
-        return new JsonResponse(['message' => 'User board updated',], 200);
+        return new JsonResponse($usersArray, 200);
     }
 
 
-    private function updateUserInformation(
-        $user,
-        $data,
-        UserPasswordHasherInterface $passwordHasher
-    ) {
-        if (isset($data['name'])) {
-            $user->setName($data['name']);
+    // ---------- PRODUCT CRUD ADMIN ----------
+
+    /**
+     * @Route("/admin/products", name="getAllProducts", methods={"GET"})
+     */
+    public function getAllProducts(
+        ProductRepository $productRepository,
+        GenreRepository $genreRepository,
+        StyleRepository $styleRepository
+    ): JsonResponse {
+        $products = $productRepository->findAll();
+
+        $productsArray = [];
+
+        foreach ($products as $product) {
+            $genres = $product->getGenre()->toArray();
+            $styles = $product->getStyle()->toArray();
+
+            $genresArray = array_map(function ($genre) {
+                return ['name' => $genre->getName()];
+            }, $genres);
+
+            $stylesArray = array_map(function ($style) {
+                return ['name' => $style->getName()];
+            }, $styles);
+
+            $productsArray[] = [
+                'id' => $product->getId(),
+                'image' => $product->getImage(),
+                'name' => $product->getName(),
+                'slug' => $product->getSlug(),
+                'description' => $product->getDescription(),
+                'price' => $product->getPrice(),
+                'quantity' => $product->getQuantity(),
+                'type' => $product->getType(),
+                'label' => $product->getLabel(),
+                'year' => $product->getYear(),
+                'country' => $product->getCountry(),
+                'format' => $product->getFormat(),
+                'artist' => $product->getArtist(),
+                'genre' => $genresArray,
+                'style' => $stylesArray,
+            ];
         }
-        if (isset($data['email'])) {
-            $user->setEmail($data['email']);
-        }
-        if (isset($data['password'])) {
-            $user->setPassword($passwordHasher->hashPassword($user, $data['password']));
-        }
-        if (isset($data['address'])) {
-            $user->setAddress($data['address']);
-        }
-        if (isset($data['phone'])) {
-            $user->setPhone($data['phone']);
-        }
+
+        return new JsonResponse($productsArray, 200);
     }
 }
