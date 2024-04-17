4438c2b066f95fad2d2bac4739ec2dbf1a304eb5
Axel <axel.valentinbvs@yahoo.com>
2024-04-17 11:30:56 +0200

    feat: SEO on img

--- frontend/src/components/ProductDetail/ProductDetail.component.tsx
+++ frontend/src/components/ProductDetail/ProductDetail.component.tsx
@@ -10,6 +10,7 @@ import "./ProductDetail.css";
 const ProductDetail = () => {
   const [thisProduct, setThisProduct] = useState<IProduct | null>(null);
   const getProductId = useParams();
+  console.log(getProductId.productId);
 
   if (getProductId.productId === undefined) {
     try {
@@ -20,11 +21,9 @@ const ProductDetail = () => {
   }
 
   const productId: number = parseInt(getProductId.productId);
-
   useEffect(() => {
     const fetchProduct = async () => {
       const product = await shopService.getProductById(productId);
-
       setThisProduct(product);
     };
 
@@ -45,10 +44,8 @@ const ProductDetail = () => {
         <div className="product-detail-images">
           <img
             className="PlaceholderImage"
-            src={
-              (thisProduct.image =
-                thisProduct.image || "https://via.placeholder.com/648x624")
-            }
+            src={thisProduct.image || "https://via.placeholder.com/648x624"}
+            alt={thisProduct.name}
           />
           <div className="Column">
             <div className="Row">
@@ -89,7 +86,7 @@ const ProductDetail = () => {
           </div>
         </div>
         <div className="product-detail-informations">
-          <p className="product-name">{thisProduct.name}</p>
+          <h1 className="product-name">{thisProduct.name}</h1>{" "}
           <p className="product-price">{thisProduct.price}€</p>
         </div>
         <div className="product-detail-description">
@@ -128,7 +125,7 @@ const ProductDetail = () => {
         <button
           onClick={() =>
             cartService.addToCart(
-              thisProduct.id,
+              thisProduct.id as number,
               thisProduct.name as string,
               thisProduct.image,
               thisProduct.price as number
