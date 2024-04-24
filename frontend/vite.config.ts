d31202f6e7d82264653b3275a3844bf9cbefeef5
Axel <axel.valentinbvs@yahoo.com>
2024-04-24 13:21:16 +0200

    prod: docker config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -9,4 +9,14 @@ export default defineConfig({
     globals: true,
     environment: "jsdom",
   },
+  preview: {
+    port: 5200,
+    strictPort: true,
+  },
+  server: {
+    port: 5200,
+    strictPort: true,
+    host: true,
+    origin: "http://0.0.0.0:5200",
+  },
 });
