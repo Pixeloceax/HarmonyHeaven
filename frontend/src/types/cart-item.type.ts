0d8150392f99c6f41cc81d8174e57e88a8ba315f
pisha <pisha@hotmail.fr>
2024-04-24 16:14:20 +0200

    adding some properties

--- frontend/src/types/cart-item.type.ts
+++ frontend/src/types/cart-item.type.ts
@@ -1,5 +1,9 @@
 import IProduct from "./product.type";
 export default interface ICartItem {
   product: IProduct;
+  id: number;
+  name: string;
+  image: string;
+  price: number;
   quantity: number;
 }
