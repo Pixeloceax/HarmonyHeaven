dbfcf9f361b5e92a299dced71675a9817797bf5c
pisha <pisha@hotmail.fr>
2024-04-24 15:58:57 +0200

    remove on cascade deleted

--- backend/harmonyheaven/src/Entity/CartItem.php
+++ backend/harmonyheaven/src/Entity/CartItem.php
@@ -13,7 +13,7 @@ class CartItem
     #[ORM\Column]
     private ?int $id = null;
 
-    #[ORM\OneToOne(cascade: ['persist', 'remove'])]
+    #[ORM\OneToOne(cascade: ['persist'])]
     #[ORM\JoinColumn(nullable: false)]
     private ?Product $product = null;
 
