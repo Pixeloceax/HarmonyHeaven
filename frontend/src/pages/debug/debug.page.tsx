bf1b3221e62a0c19ce1aa28215830d463ac42b4a
Amir Abdallah-Khodja <amiir.qt@gmail.com>
2024-04-26 10:23:01 +0200

    structure update

--- frontend/src/pages/debug/debug.page.tsx
+++ frontend/src/pages/debug/debug.page.tsx
@@ -1,7 +1,6 @@
 import { Component } from "react";
 import Loader from "../../components/loader/loader.component";
 import Checkbox from "../../components/checkbox/checkbox.component";
-import Card from "../../components/card/card.component";
 
 import "./debug.css";
 
@@ -54,17 +53,7 @@ export default class Debug extends Component<Record<string, never>, State> {
         </li>
         <Loader />
         <Checkbox />
-        <div className="debug-card">
-          {[...Array(6)].map((_, i) => (
-            <Card
-              key={i}
-              image="https://upload.wikimedia.org/wikipedia/en/d/df/Gorillaz_Demon_Days.PNG"
-              price={20}
-              artist="Gorillaz"
-              album="Demon Days"
-            />
-          ))}
-        </div>
+
 
         <div>
           {fontSizes.map((size, index) => (
